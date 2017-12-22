5f176e52d414204afe10e585f70fbbe730a84647
A. Thompson <act.webdesigns@gmail.com>
2017-12-22 17:58:00 -0600

    initial wireframe, Initial Firebase setup & added images

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rock, Paper, Sciccors
+## Classic Rock, Paper, Scissors played live over the internet.
+Powered by Firebase
---
+++ assets/app.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCYWVZBBlcCTjQsGcEyLoe_gayyZXoS2Fg",
+    authDomain: "rpb-smu.firebaseapp.com",
+    databaseURL: "https://rpb-smu.firebaseio.com",
+    projectId: "rpb-smu",
+    storageBucket: "rpb-smu.appspot.com",
+    messagingSenderId: "597172240588"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+
