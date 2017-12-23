5d32b7b6f01f78f70bd54fe01ee9ebb7bfdb88ad
Andrew T <act.webdesigns@gmail.com>
2017-12-23 17:22:52 -0600

    added .gitignor

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,21 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyCYWVZBBlcCTjQsGcEyLoe_gayyZXoS2Fg",
-    authDomain: "rpb-smu.firebaseapp.com",
-    databaseURL: "https://rpb-smu.firebaseio.com",
-    projectId: "rpb-smu",
-    storageBucket: "rpb-smu.appspot.com",
-    messagingSenderId: "597172240588"
-};
-firebase.initializeApp(config);
-var database = firebase.database();
+        apiKey: 'AIzaSyCYWVZBBlcCTjQsGcEyLoe_gayyZXoS2Fg',
+        authDomain: 'rpb-smu.firebaseapp.com',
+        databaseURL: 'https://rpb-smu.firebaseio.com',
+        projectId: 'rpb-smu',
+        storageBucket: 'rpb-smu.appspot.com',
+        messagingSenderId: '597172240588',
+}
+firebase.initializeApp(config)
+var database = firebase.database()
 
+var playerOne = $('#player-one')
+var playerTwo = $('#plaer-two')
+var playerOneSelectedChoice = $('.player-one-selected-choice')
+var playerTwoSelectedChoice = $('.player-two-selected-choice')
+
+$('.answer').on("click", function() {
+	console.log($("this").val())
+	
+})
