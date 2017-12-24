8111a72624915f32267a92a299f19a4cd972a9c2
Andrew T <act.webdedigns@gmail.com>
2017-12-23 21:54:20 -0600

    click function added

--- assets/app.js
+++ assets/app.js
@@ -1,21 +1,28 @@
 // Initialize Firebase
 var config = {
-        apiKey: 'AIzaSyCYWVZBBlcCTjQsGcEyLoe_gayyZXoS2Fg',
-        authDomain: 'rpb-smu.firebaseapp.com',
-        databaseURL: 'https://rpb-smu.firebaseio.com',
-        projectId: 'rpb-smu',
-        storageBucket: 'rpb-smu.appspot.com',
-        messagingSenderId: '597172240588',
+    apiKey: 'AIzaSyCYWVZBBlcCTjQsGcEyLoe_gayyZXoS2Fg',
+    authDomain: 'rpb-smu.firebaseapp.com',
+    databaseURL: 'https://rpb-smu.firebaseio.com',
+    projectId: 'rpb-smu',
+    storageBucket: 'rpb-smu.appspot.com',
+    messagingSenderId: '597172240588',
 }
 firebase.initializeApp(config)
 var database = firebase.database()
 
-var playerOne = $('#player-one')
-var playerTwo = $('#plaer-two')
-var playerOneSelectedChoice = $('.player-one-selected-choice')
-var playerTwoSelectedChoice = $('.player-two-selected-choice')
-
-$('.answer').on("click", function() {
-	console.log($("this").val())
-	
+var p1Selected, p2Selected
+$(document).on("click", ".answer-choice", function() {
+    var rps = $(this).attr("alt")
+    rps = rps.charAt(0).toUpperCase() + rps.slice(1)
+    if ($(this).attr("player") == "1") {
+        $("#player-one-selected-choice").html("<h5>" + rps + "</h5>")
+	return p1Selected = true
+    } else {
+        $("#player-two-selected-choice").html("<h5>" + rps + "</h5>")
+	return p2selected = true
+    }
 })
+function compare(p1Choice, p2Choice) {
+	//TODO: make switch case for answers
+}
+//TODO: Add Firebase database calls, Add check for 2 players
