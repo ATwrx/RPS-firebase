939f2e373ae493c97e4776474f22f6197f09943b
Andrew T <act.webdedigns@gmail.com>
2017-12-31 20:06:24 -0600

    it works TODO: Add firebase backend

--- assets/app.js
+++ assets/app.js
@@ -19,33 +19,39 @@ const p2 = {
     choice: null
 }
 $(document).on("click", ".answer-choice", function() {
-    let rps = $(this).attr("alt"){
+    let rps = $(this).attr("alt") 
+     if($(this).attr("player") == 1) {
+	$(this).siblings().removeClass("selected")
         p1.choice = rps
+	$(this).addClass("selected")
     } else {
+	$(this).siblings().removeClass("selected")
         p2.choice = rps
+	$(this).addClass("selected")
     }
 })
 $(document).on("click", "#player-one-submit", function() {
     p1.hasChosen = true
-    p1.finalChoice = p2.choice
     $("#player-one-submit").hide()
-    $("#player-one-selected-choice").html("<h5>" + player1.finalChoice.charAt(0).toUpperCase() + player1.finalChoice.slice(1)
-psPretty + "</h5>")
-    return p1.hasChosen, p1.finalChoice
+    $("#player-one-selected-choice").html("<h5>" + p1.choice.charAt(0).toUpperCase() + p1.choice.slice(1)
+    + "</h5>")
+    compare(p1.choice, p2.choice)
+    return p1.hasChosen 
 })
 
 $(document).on("click", "#player-two-submit", function() {
     p2.hasChosen = true
-    p2.finalChoice = p2.choice
     $("#player-two-submit").hide()
-    $("#player-two-selected-choice").html("<h5>" + rpsPretty + "</h5>")
-    return p2.hasChosen, p2finalChoice
+    $("#player-two-selected-choice").html("<h5>" + p2.choice.charAt(0).toUpperCase() + p2.choice.slice(1)+ "</h5>")
+    return p2.hasChosen 
 })
 
 function compare(choice1, choice2) {
     let rBox = $("#battle-box")
     let $h4 = $("<h4>")
-
+    if (p1.hasChosen == false || p2.hasChosen == false){
+    return
+    }
     if (choice1 == choice2) {
         $h4.text("The result is a tie!")
     } else if (choice1 == "rock") {
